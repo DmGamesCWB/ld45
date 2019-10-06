8779c849639462c2b66f3d588b6629d8ac8b664a
Fabricio Brito <fabricio.brito@gmail.com>
2019-10-06 19:41:31 -0300

    Load MainMenu after end of credits

---
+++ Ludum Dare #45/Assets/Scripts/LoadSceneAfterTimeout.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneAfterTimeout : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(20);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
