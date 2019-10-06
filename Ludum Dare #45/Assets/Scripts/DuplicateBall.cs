3f329ed7972d86a827d7cc6c886987261fc3b10b
Gustavo Heimovski <gbashei@gmail.com>
2019-10-06 16:02:22 -0300

    Level8: Doubling
    Signed-off-by: Gustavo Heimovski <gbashei@gmail.com>

--- Ludum Dare #45/Assets/Scripts/DuplicateBall.cs
+++ Ludum Dare #45/Assets/Scripts/DuplicateBall.cs
@@ -27,31 +27,21 @@ public class DuplicateBall : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
             if (hit.collider != null)
             {
-                //DuplicateBall Dball = GetComponent<DuplicateBall>();
-                //if (Dball != null)
-                //{
-                //    Dball.duplicate(this.gameObject);
-                //}
                 // Everytime the ball is hit, the score increases by one
-                
-                score.GetComponent<KeepScore>().AddScore(1);
-                hit.collider.attachedRigidbody.AddForce(
-                    new Vector2(
-                        (hit.collider.gameObject.transform.position.x - mousePos.x) * bounceForceX,
-                        bounceForceY),
-                    ForceMode2D.Impulse);
-                Instantiate(ball, new Vector3(ball.transform.position.x * 3, ball.transform.position.y + 10, ball.transform.position.z), Quaternion.identity);
+                if (!hit.collider.CompareTag("Fake") &&
+                    // If the instance of the Object is the same of the Object this component is attached to
+                    hit.collider.gameObject.GetInstanceID() == gameObject.GetInstanceID())
+                {
+                    score.GetComponent<KeepScore>().AddScore(1);
+                    hit.collider.attachedRigidbody.AddForce(
+                        new Vector2(
+                            (hit.collider.gameObject.transform.position.x - mousePos.x) * bounceForceX,
+                            bounceForceY),
+                        ForceMode2D.Impulse);
+                    Instantiate(ball, new Vector3(ball.transform.position.x + 0.3f, ball.transform.position.y + 0.3f, ball.transform.position.z), Quaternion.identity);
+                }
             }
         }
     }
 
 }
-
-    //public void duplicate(GameObject ball)
-    //{
-        // Instantiate at position (0, 0, 0) and zero rotation.
-       // Instantiate(ball, new Vector3(ball.transform.position.x * 3, ball.transform.position.y+10, ball.transform.position.z), Quaternion.identity);
-               
-        //Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-    //}
-//}
