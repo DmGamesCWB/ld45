2353db16794ec19191cf06c9b1080213b1745894
Fabricio Brito <fabricio.brito@gmail.com>
2019-10-06 18:38:47 -0300

    Generate Random size and force on Level 4

---
+++ Ludum Dare #45/Assets/Scripts/BlinkRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkRenderer : MonoBehaviour
+{
+    private float timeSinceLastRendererToggle = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastRendererToggle += Time.deltaTime;
+        Renderer ballRenderer = this.GetComponent<Renderer>();
+        if(timeSinceLastRendererToggle >= 0.1f)
+        {
+            timeSinceLastRendererToggle = 0.0f;
+            ballRenderer.enabled = !ballRenderer.enabled;
+        }
+    }
+}
