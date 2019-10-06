01596aad47de3cbf1012c5b9db4dc3994f18123d
Fabricio Brito <fabricio.brito@gmail.com>
2019-10-06 16:58:01 -0300

    Level 3 brick shrinks at every hit and score gives feedback to user

--- Ludum Dare #45/Assets/Scripts/BrickHit.cs
+++ Ludum Dare #45/Assets/Scripts/BrickHit.cs
@@ -8,7 +8,7 @@ public class BrickHit : MonoBehaviour
     private const int kMaxCollisions = 3;
     public GameObject score;
     public AudioClip hitBrickSound;
-
+    public float shrinkRate = 0.7f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,16 +20,20 @@ public class BrickHit : MonoBehaviour
     {
         if (numCollision >= kMaxCollisions)
         {
-            score.GetComponent<KeepScore>().AddScore(1);
             Debug.Log("Destroying brick");
-            Object.Destroy(this);
+            Destroy(this.gameObject);
         }
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
         Debug.Log("Brick hit");
+        score.GetComponent<KeepScore>().AddScore(1);
         AudioManager.inst.AudioPlay(hitBrickSound);
         numCollision++;
+        transform.localScale = new Vector3(
+            transform.localScale.x * shrinkRate,
+            transform.localScale.y,
+            transform.localScale.z);
     }
 }
